84e091dcdfa0201d1170519b43c2227ab9c4a909
DADA <dadaneillindar@gmail.com>
2014-02-10 11:35:10 +0800

    add start video

--- src/com/example/test01/MainActivity.java
+++ src/com/example/test01/MainActivity.java
@@ -1,122 +1,52 @@
 package com.example.test01;
 
-import java.util.Locale;
-import java.util.Random;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
+import android.view.Menu;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-public class MainActivity extends Activity{
-	
-	TextToSpeech tts;
-	
+public class MainActivity extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-	
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-	       /*getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-	                  WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
 		setContentView(R.layout.activity_main);
 		
-		ImageButton bt1 = (ImageButton)this.findViewById(R.id.imagebutton1);
+		final VideoView videoView = (VideoView) this.findViewById(R.id.videoview);
+		MediaController mc = new MediaController(this);
+		videoView.setMediaController(mc);
+		
+		videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.nitro_logo));   
+		videoView.requestFocus();
+		videoView.start();
+		
+		Button bt1 = (Button)this.findViewById(R.id.button1);
+		
 		bt1.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v1) {
-				
-				Intent intent = new Intent(v1.getContext(),MainActivity_Search.class);
-				startActivity(intent);
-			}
-		});
-		
-		bt1.setOnLongClickListener(new View.OnLongClickListener() {
-			
-			@Override
-			public boolean onLongClick(View v) {
-				
-				tts.speak("Search", TextToSpeech.QUEUE_FLUSH, null);		
-				return true;
-			}
-		});
-		
-		ImageButton bt2 = (ImageButton)this.findViewById(R.id.imagebutton2);
-		bt2.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v2) {
-		
-				Intent intent = new Intent(v2.getContext(),MainActivity_NowPosition.class);
-				startActivity(intent);
-			}
-		});
-		
-        bt2.setOnLongClickListener(new View.OnLongClickListener() {
-			
-			@Override
-			public boolean onLongClick(View v) {
-				
-				tts.speak("Now Position", TextToSpeech.QUEUE_FLUSH, null);		
-				return true;
-			}
-		});
-		
-		ImageButton bt3 = (ImageButton)this.findViewById(R.id.imagebutton3);
-		bt3.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v3) {
-				
-				Intent intent = new Intent(v3.getContext(),MainActivity_Other.class);
-				startActivity(intent);
-			}
-		});
-		
-        bt3.setOnLongClickListener(new View.OnLongClickListener() {
-			
-			@Override
-			public boolean onLongClick(View v) {
-				
-				tts.speak("Other", TextToSpeech.QUEUE_FLUSH, null);	
-				return true;
-			}
-		});
-		
-		ImageButton bt4 = (ImageButton)this.findViewById(R.id.imagebutton4);
-		bt4.setOnClickListener(new View.OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//要做的事
 				
-				finish();
+				Intent intent = new Intent(v.getContext(),MainActivity_Home.class);
+				startActivity(intent);
 			}
 		});
-	
-        bt4.setOnLongClickListener(new View.OnLongClickListener() {
-			
-			@Override
-			public boolean onLongClick(View v) {
-				
-				tts.speak("Quit", TextToSpeech.QUEUE_FLUSH, null);
-				return true;
-			}
-		});
-
-		tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-			
-			@Override
-			public void onInit(int status) {
-				
-				if(status != TextToSpeech.ERROR){
-					tts.setLanguage(Locale.ENGLISH);
-				}
-			}
-		});
+		
+		
+		
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
---
+++ src/com/example/test01/MainActivity_Home.java
@@ -0,0 +1,122 @@
+package com.example.test01;
+
+import java.util.Locale;
+import java.util.Random;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity_Home extends Activity{
+	
+	TextToSpeech tts;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+	       /*getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+	                  WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
+		setContentView(R.layout.activity_home);
+		
+		ImageButton bt1 = (ImageButton)this.findViewById(R.id.imagebutton1);
+		bt1.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v1) {
+				
+				Intent intent = new Intent(v1.getContext(),MainActivity_Search.class);
+				startActivity(intent);
+			}
+		});
+		
+		bt1.setOnLongClickListener(new View.OnLongClickListener() {
+			
+			@Override
+			public boolean onLongClick(View v) {
+				
+				tts.speak("Search", TextToSpeech.QUEUE_FLUSH, null);		
+				return true;
+			}
+		});
+		
+		ImageButton bt2 = (ImageButton)this.findViewById(R.id.imagebutton2);
+		bt2.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v2) {
+		
+				Intent intent = new Intent(v2.getContext(),MainActivity_NowPosition.class);
+				startActivity(intent);
+			}
+		});
+		
+        bt2.setOnLongClickListener(new View.OnLongClickListener() {
+			
+			@Override
+			public boolean onLongClick(View v) {
+				
+				tts.speak("Now Position", TextToSpeech.QUEUE_FLUSH, null);		
+				return true;
+			}
+		});
+		
+		ImageButton bt3 = (ImageButton)this.findViewById(R.id.imagebutton3);
+		bt3.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v3) {
+				
+				Intent intent = new Intent(v3.getContext(),MainActivity_Other.class);
+				startActivity(intent);
+			}
+		});
+		
+        bt3.setOnLongClickListener(new View.OnLongClickListener() {
+			
+			@Override
+			public boolean onLongClick(View v) {
+				
+				tts.speak("Other", TextToSpeech.QUEUE_FLUSH, null);	
+				return true;
+			}
+		});
+		
+		ImageButton bt4 = (ImageButton)this.findViewById(R.id.imagebutton4);
+		bt4.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				finish();
+			}
+		});
+	
+        bt4.setOnLongClickListener(new View.OnLongClickListener() {
+			
+			@Override
+			public boolean onLongClick(View v) {
+				
+				tts.speak("Quit", TextToSpeech.QUEUE_FLUSH, null);
+				return true;
+			}
+		});
+
+		tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+			
+			@Override
+			public void onInit(int status) {
+				
+				if(status != TextToSpeech.ERROR){
+					tts.setLanguage(Locale.ENGLISH);
+				}
+			}
+		});
+	}
+}
